5929395a7b8b89ab3e5c997db93b653ffd9f059d
Josh Rog <jrogerson>
2022-11-08 11:18:05 -0800

    Utilizing while and for loops in javascript

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+// alert("Hello, yoyoyoyyoyooyoyoyoy")
+// console.log("Hello there")
+
+// for (let i = 2; i <= 10 ; i++)
+// {
+
+//     console.log(2 ** i)
+
+// } 
+
+// let i = 1;
+
+// while (i <= 10) {
+    
+//     console.log(Math.pow(2,i))
+//     i++;
+// }
+
