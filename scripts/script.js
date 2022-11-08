2ec57aaa3876b2d273385d4746aac202d6d91595
Josh Rog <jrogerson>
2022-11-08 11:11:36 -0800

    objects and arrays in javascript

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,94 @@
 // alert("Hello, yoyoyoyyoyooyoyoyoy")
-console.log("Hello there")
+// console.log("Hello there")
 
-for (let i = 2; i <= 10 ; i++)
-{
+// for (let i = 2; i <= 10 ; i++)
+// {
 
-    console.log(2 ** i)
+//     console.log(2 ** i)
 
-} 
+// } 
 
-let i = 1;
+// let i = 1;
 
-while (i <= 10) {
+// while (i <= 10) {
     
-    console.log(Math.pow(2,i))
-    i++;
+//     console.log(Math.pow(2,i))
+//     i++;
+// }
+
+// let input = "";
+// do {
+//     input = prompt("What is your name");
+    
+    
+
+//   } while (!isNaN(Number(input)));
+  
+//   console.log(input);
+
+//   alert("Welcome" + input);
+//   alert(`Welcome ${input}`);
+
+  let fruits = ["Apple", "Banana"];
+  fruits.push("Orange");
+  console.log(fruits);
+
+  fruits.pop();
+  console.log(fruits);
+
+  fruits.unshift("Mango");
+  console.log(fruits);
+  
+  fruits.shift();
+  console.log(fruits)
+
+  for(let item of fruits)
+  {
+
+    console.log
+  }
+
+  const person ={
+name:['Bob', 'Smith'],
+age:32,
+gender:'male',
+interests: ['music', 'skiing']  }
+
+console.log(`My name is ${person.name[0]} ${person.name[1]} i am interested in ${person.interests[0]} and ${person.interests[1]}`)
+
+
+let student1 = {
+    ID: "A00022",
+    GPA: 3.0,
+    program: "CIT"
+};
+
+let student2 = {
+    ID: "A01000",
+    GPA: 3.1,
+    program: "CST"
+};
+
+let student3 = {
+    ID: "A00114",
+    GPA: 3.2,
+    program: "CIT"
+};
+let students = [student1, student2, student3]; 
+let counter = 0;
+for(let stud of students){
+    
+    if(student1.program === "CIT" && stud.GPA > 3){
+
+        counter++
+    }
+
 }
 
-let input = "";
-do {
-    input = prompt("What is your name");
-    
-    
+console.log(counter)
 
-  } while (!isNaN(Number(input)));
-  
-  console.log(input);
+for(let i = 0; i <students.length; i++)
+{
 
-  alert("Welcome" + input);
-  alert(`Welcome ${input}`);
+    if (students)[i].program
+}
